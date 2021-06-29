162056d3ad5a4ac156763ed4477329a8d5d16ca3
Coco Puff <fahrija_wyroski@live.com>
2021-06-29 18:07:26 -0500

    added basic setup and some user model code

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use(require('./routes'));
+
+//set up Mongoose to connect when we start the app
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/lets_get_social', {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+//****USE THIS TO LOG MONGO QUERIES BEING EXECUTED!****
+mongoose.set('debug', true);
+
+
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
