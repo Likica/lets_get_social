c04067e12909a5847b2be0a3a25d1049be8b83a9
Coco Puff <fahrija_wyroski@live.com>
2021-06-29 17:53:46 -0500

    initial setup

---
+++ models/Thought.js
@@ -0,0 +1,67 @@
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const ReactionSchema = new Schema(
+    {
+        // set custom id to avoid confusion with parent thought's _id field
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            // trim: true // need to be 280 character maximum
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
+
+const ThoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            //mustBe between 1 and 280 characters
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        // use ReplySchema to validate data for a reply
+        reactions: [ReactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+ThoughtSchema.virtual('reactionCount').get(function () {
+    return this.reaction.length;
+});
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
---
+++ models/User.js
@@ -0,0 +1,47 @@
+const UserSchema = new Schema({
+    userName: {
+        type: String,
+        // need to add *** UNIQUE 
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        // need to add *** UNIQUE 
+
+        // need to add *** VALIDATION must match a valid email address (Mongoose's matching validation) 
+    },
+    thoughts: {
+        type: String,
+        //Array of _id values referencing the Thought model
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => dateFormat(createdAtVal)
+    },
+    friends: [
+        {
+            //Array of _id values referencing the User model (self-reference)
+        }
+    ]
+},
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        }
+    }
+);
+//create a virtual called 'friendCount' that retrieves the length of the user's 'frinds' array field on query
+UserSchema.virtual('friendCount').get(function () {
+    return this.friends.reduce((total, friends) => total + friends.length + 1, 0);
+});
+
+
+//vreate the Pizza model using the PizzaSchema
+const User = model('User', UserSchema);
+
+//export the Pizza Model
+module.exports = User;
